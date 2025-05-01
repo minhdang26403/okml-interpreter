9ff8790cbc4ff9512fb359813d5f69e55cd9838b
Dang Truong <truongminhdang26403@gmail.com>
2025-04-30 21:30:52 -0400

    feat(testing): implement a REPL program to support testing

--- bin/repl.ml
+++ bin/repl.ml
@@ -1,23 +1,37 @@
-(* repl.ml *)
 open Interpreter
 
-let rec repl () =
+let read_input () =
   print_string "OKml> ";
   flush stdout;
   match read_line () with
   | exception End_of_file ->
       print_endline "\nExiting OKml REPL.";
       exit 0
-  | line -> (
-      let lexbuf = Lexing.from_string line in
-      try
-        let ast = Parser.parse Lexer.tokenize lexbuf in
-        print_endline ("AST: " ^ Exp.string_of_ast ast);
+  | line ->
+      let trimmed = String.trim line in
+      if trimmed = ":q" then (
+        print_endline "Exiting OKml REPL.";
+        exit 0)
+      else if trimmed = "" then (
+        "")
+      else (
+        trimmed)
+
+let rec repl () =
+  let input = read_input () in
+  if input <> "" then (
+    let lexbuf = Lexing.from_string input in
+    let ast = Parser.parse Lexer.tokenize lexbuf in
+    print_endline ("AST: " ^ Exp.string_of_ast ast);
+    match Typechecker.infer ast with
+    | None ->
+        print_endline "Type error: No valid type could be inferred";
         repl ()
-      with Parsing.Parse_error ->
-        let pos = lexbuf.lex_curr_p in
-        Printf.printf "Parse error at line %d, column %d\n%!" pos.pos_lnum
-          (pos.pos_cnum - pos.pos_bol);
-        repl ())
+    | Some ty ->
+        print_endline ("Type: " ^ Type.string_of_ty ty);
+        let value = Eval.eval ast in
+        print_endline ("Value: " ^ Exp.string_of_ast value);
+        print_newline ());
+  repl ()
 
 let () = repl ()
